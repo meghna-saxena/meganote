17e1b13b56b677e39dd06dc21cc96ad2ce5bda98
Meghna Srivastava <meghna.s1906@gmail.com>
2018-08-16 16:25:46 +0200

    saving user model instances for facebook records

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,9 +1,28 @@
 const passport = require("passport");
 const TwitterStrategy = require("passport-twitter").Strategy;
 const FacebookStrategy = require("passport-facebook").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
+//requiring mongoose model in this file
+//User is model class
+const User = mongoose.model("users");
+
 //creates new instance of twitter/fb passport strategy
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: keys.facebookAppId,
+      clientSecret: keys.facebookAppSecret,
+      callbackURL: "/auth/facebook/callback",
+      profileFields: ["id", "email", "displayName", "photos"]
+    },
+    (accessToken, refreshToken, profile, done) => {
+      new User({ facebookId: profile.id, name: profile.displayName, emails: profile.emails }).save();
+    }
+  )
+);
+
 passport.use(
   new TwitterStrategy(
     {
@@ -15,22 +34,6 @@ passport.use(
       console.log("twitter oauth_token", oauth_token);
       console.log("twitter tokenSecret", tokenSecret);
       console.log("twitter profile", profile);
-    }
-  )
-);
-
-passport.use(
-  new FacebookStrategy(
-    {
-      clientID: keys.facebookAppId,
-      clientSecret: keys.facebookAppSecret,
-      callbackURL: "/auth/facebook/callback",
-      profileFields: ["id", "email", "displayName", "photos"]
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("facebook accessToken", accessToken);
-      console.log("facebook refreshToken", refreshToken);
-      console.log("facebook profile", profile);
     }
   )
 );
